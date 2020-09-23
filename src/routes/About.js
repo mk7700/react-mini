560aeca22afb8063fb62ddccce516a7acdd7d7ae
mk7700 <66050813+mk7700@users.noreply.github.com>
2020-09-23 15:17:00 +0900

    maybe_final

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About() {    
+    return (
+        <div className="about__container">
+            <span>
+                "About this page: I build it because I learned React"
+            </span>
+            <span>mk7700</span>
+        </div>
+    );
+}
+
+export default About;
